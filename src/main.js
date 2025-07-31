8ca35644b94538fb1dcb84300ef291b580b68bdb
Pavel <Pashaz1994@gmail.com>
2025-07-31 18:44:41 +0300

    31/07/25

--- src/main.js
+++ src/main.js
@@ -112,8 +112,8 @@ function analyzeSalesData(data, options) {
   return sellerStats.map((seller) => ({
     seller_id: seller.id,
     name: seller.name,
-    revenue: seller.revenue.toFixed(2),
-    profit: seller.profit.toFixed(2),
+    revenue: +seller.revenue.toFixed(2),
+    profit: +seller.profit.toFixed(2),
     sales_count: seller.sales_count,
     top_products: seller.top_products,
     bonus: +seller.bonus.toFixed(2),
